c51fad4cc23f6e025eca46e6331f49000f8cbdc7
Widya1308 <220103253@mhs.udb.ac.id>
2025-12-22 20:22:27 +0700

    notif
    notif

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
+import NotificationBadge from "./NotificationBadge";
+
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-dark bg-dark px-3">
+    <nav className="navbar navbar-dark bg-danger px-3 d-flex justify-content-between">
       <span className="navbar-brand">
         Sistem Pengaduan Sarpras
       </span>
-      <button className="btn btn-outline-light btn-sm">
-        Sign out
-      </button>
+
+      <div className="d-flex align-items-center">
+        <NotificationBadge />
+        <button className="btn btn-outline-light btn-sm">
+          Sign out
+        </button>
+      </div>
     </nav>
   );
 }
